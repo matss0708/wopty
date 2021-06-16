3c70cd8c50d2c41372349735eb986267961746b4
ch.faraz <ch.faraz324@gmail.com>
2021-06-16 16:50:09 +0500

    logo changed

--- components/header.js
+++ components/header.js
@@ -1,7 +1,7 @@
 const Header = () => {
     return (
         <div className="flex justify-center pt-5">
-            <img src="/APM_logo_Symbool_Zwart.png" alt="logo" className="lg:w-24 lg:h-24 w-16 h-16 mr-5 lg:mb-5" />
+            <img src="/logo.png" alt="logo" className="lg:w-24 lg:h-24 w-16 h-16 lg:mb-5" />
             {/* <div className="text-center text-4xl font-bold py-5 text-white">Wall Of Positivity</div>; */}
         </div>
     );
