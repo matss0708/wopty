b0a90d488af37847ae40c78e7d2bc90975af674e
ch.faraz <ch.faraz324@gmail.com>
2021-06-18 17:51:03 +0500

    fixed header and rendom colors to messages added

--- components/header.js
+++ components/header.js
@@ -1,7 +1,19 @@
 const Header = () => {
     return (
-        <div className="flex justify-center pt-5">
-            <img src="/logo.png" alt="logo" className="lg:w-24 lg:h-24 w-16 h-16 lg:mb-5" />
+        <div className="px-16 pb-4 lg:pb-0 flex fixed justify-between pt-5 z-10 bg-black w-full">
+            <img src="/logo.png" alt="logo" className="lg:w-20 lg:h-20 w-14 h-14 lg:mb-5" />
+            <div className="flex justify-between mt-3 lg:mt-5 w-32">
+                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-instagram" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                    <rect x="4" y="4" width="16" height="16" rx="4" />
+                    <circle cx="12" cy="12" r="3" />
+                    <line x1="16.5" y1="7.5" x2="16.5" y2="7.501" />
+                </svg>
+                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-facebook" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                    <path d="M7 10v4h3v7h4v-7h3l1 -4h-4v-2a1 1 0 0 1 1 -1h3v-4h-3a5 5 0 0 0 -5 5v2h-3" />
+                </svg>
+            </div>
             {/* <div className="text-center text-4xl font-bold py-5 text-white">Wall Of Positivity</div>; */}
         </div>
     );
--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,7 @@ export default function Home() {
     const [mobilePosition, setMobilePosition] = useState([]);
 
     const [rotate, setRotate] = useState(['rotate-1', 'rotate-2', 'rotate-3', 'rotate-6', 'rotate-12', 'rotate-12', '-rotate-12', '-rotate-12', '-rotate-6', '-rotate-3', '-rotate-2', '-rotate-1']);
-    const [color, setColor] = useState(['#D50000', '#C51162', '#AA00FF', '#6200EA', '#304FFE', '#0091EA', '#00B8D4', '#00BFA5', '#00C853', '#64DD17', '#FFD600', '#FFAB00', '#FF6D00', '#DD2C00', '#6D4C41', '#757575', '#263238', '#78909C']);
+    const [color, setColor] = useState(['red', 'green', 'blue', 'purple', 'black']);
 
     const [chunked, setChunked] = useState([]);
     useEffect(async () => {
@@ -33,7 +33,10 @@ export default function Home() {
         }
         shuffleArray(position);
 
-        document.body.style.backgroundColor = color[background];
+        // document.body.style.backgroundColor = color[background];
+        // if(document.readyState){
+        //     document.getElementById('message').style.color = color[background];
+        // }
         const res = await fetch(`/api/post`);
         const data = await res.json();
         shuffleArray(data);
@@ -69,7 +72,7 @@ export default function Home() {
                     <link rel="icon" href="/favicon.ico" />
                 </Head>
                 <Header />
-                <div className="mt-1 w-full h-5/6 main overflow-y-scroll md:overflow-y-visible ">
+                <div className="pt-24 lg:pt-36 w-full h-5/6 main overflow-y-scroll md:overflow-y-visible ">
                     {width <= 768 ? (
                         <Carousel showThumbs={false} autoPlay={true} showArrows={false} infiniteLoop={true} showStatus={false}>
                             {chunked.length &&
@@ -79,7 +82,7 @@ export default function Home() {
                                             {post.length &&
                                                 post.map((onePost, index) => {
                                                     return (
-                                                        <p key={onePost._id} className={`bg-white text-black my-5 post max-w-20 transform min-h-8 mx-8 bg-white p-3 rounded-md ${fonts[Math.round(Math.random() * 4)]} ${fontWeight[Math.round(Math.random() * 5)]}`}>
+                                                        <p  key={onePost._id} className={`bg-white ${color[Math.round(Math.random() * 4)]} my-5 post max-w-20 transform min-h-8 mx-8 bg-white p-3 rounded-md ${fonts[Math.round(Math.random() * 4)]} ${fontWeight[Math.round(Math.random() * 5)]}`}>
                                                             {onePost.post}
                                                         </p>
                                                     );
@@ -93,7 +96,7 @@ export default function Home() {
                         posts.map((post, index) => {
                             return (
                                 <div style={{ gridArea: position[index] }} key={post._id}>
-                                    <p className={`bg-white text-black mt-5 post max-w-20 transform min-h-8 mx-8 bg-white p-3 rounded-md ${fonts[Math.round(Math.random() * 5)]} ${fontWeight[Math.round(Math.random() * 4)]} transition duration-500 ease-in-out hover:scale-110 shadow-lg hover:shadow-none message`}>{post.post}</p>
+                                    <p className={`bg-white ${color[Math.round(Math.random() * 4)]} mt-5 post max-w-20 transform min-h-8 mx-8 bg-white p-3 rounded-md ${fonts[Math.round(Math.random() * 5)]} ${fontWeight[Math.round(Math.random() * 4)]} transition duration-500 ease-in-out hover:scale-110 shadow-lg hover:shadow-none message`}>{post.post}</p>
                                 </div>
                                 // font-${fonts[Math.round(Math.random() * 5)]}
                             );
